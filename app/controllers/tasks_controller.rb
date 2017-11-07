d2f9761035bd2368a78048bccf438529c17a5f62
romain hallary <niamor@mm.st>
2017-11-07 15:12:05 +0000

    task manager completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,42 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+   def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    # Filtering/Whitelisting the params that come in from the form
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
